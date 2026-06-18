a4731149a8f670234771ae76769bf2bc599934e9
Vishal-Misrha <official1vishal7@gmail.com>
2026-06-18 22:14:30 +0530

    class work

---
+++ frist.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+console.log("This is my first JavaScript file.");
+name = "vishal";
+console.log (name);
+price="99.90";
+console.log(price);
+x= null;
+y= undefined;
+console.log(y);
+isFollow =true;
+console.log(isFollow)
