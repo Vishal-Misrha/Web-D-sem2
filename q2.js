7de24544b137bc04b54998682598ca607f9a06e4
Vishal-Misrha <official1vishal7@gmail.com>
2026-06-18 22:16:10 +0530

    class work

---
+++ q2.js
@@ -0,0 +1,20 @@
+/*let num = prompt("enter a number");
+if (num % 5===0){
+    console.log(num,"is divisible by 5");
+}else{
+    console.log(num,"is not divisible by 5");
+}*/
+let marks=22;
+if (marks >=80&& marks <=100){
+    console.log("A grade");
+}else if (marks >=70 && marks <89){
+    console.log("B grade");
+
+}
+else if (marks >=60 && marks <69){
+    console.log("C grade");
+}else if (marks >=50 && marks <59){
+    console.log ("d grade")
+}else { 
+    console.log("F grade")
+}
